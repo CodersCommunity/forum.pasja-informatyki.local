af8970028869b059b34b74c58c429361f29aa4b7
Krzysztof Pazdur <pazdurk@gmail.com>
2016-10-10 22:11:01 +0200

    Refactor main CKEditor class

--- forum/qa-plugin/ckeditor4/qa-plugin.php
+++ forum/qa-plugin/ckeditor4/qa-plugin.php
@@ -1,15 +1,16 @@
 <?php
 /*
-	Plugin Name: CKEditor4
+	Plugin Name: CKEditor
 	Plugin URI: 
-	Plugin Description: Wrapper for CKEditor4 WYSIWYG rich text editor
+	Plugin Description: New Editor for Forum.
 	Plugin Version: 1.6
-	Plugin Date: 2015-02-13
-	Plugin Author: sama55@CMSBOX mod efik <pazdurk@gmail.com>
-	Plugin Author URI: http://www.cmsbox.jp/
+	Plugin Date: 2016-10-10
+	Plugin Author:  CodersCommunity
+	Plugin Author https://github.com/CodersCommunity
 	Plugin License: GPLv2
 	Plugin Minimum Question2Answer Version: 1.5.1
-	Plugin Update Check URI: 
+	Plugin Update Check URI:
+    Oryginal Author: sama55@CMSBOX (http://www.cmsbox.jp/) Based on his work.
 */
 
 if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
@@ -18,11 +19,7 @@ if (!defined('QA_VERSION')) { // don't allow this page to be requested directly 
 }
 
 qa_register_plugin_phrases('qa-lang/qa-ckeditor4-lang-*.php', 'ck4');
-qa_register_plugin_module('editor', 'qa-ckeditor4.php', 'qa_ckeditor4', 'CKEditor4');
+qa_register_plugin_module('editor', 'CkEditor.php', 'CkEditor', 'CKEditor4');
 qa_register_plugin_module('page', 'qa-ckeditor4-upload.php', 'qa_ckeditor4_upload', 'CKEditor4 Upload');
 qa_register_plugin_layer('qa-ckeditor4-layer.php', 'CKEditor4 Layer');
 qa_register_plugin_overrides('qa-ckeditor4-overrides.php');
-
-/*
-	Omit PHP closing tag to help avoid accidental output
-*/
