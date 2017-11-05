bce0718a3089e20a3e3fc66a6845831ff918c9a2
Patrycjerz <patrykkluk@gmail.com>
2017-11-05 13:47:49 +0100

    Create simple plugin for tips about forum

---
+++ forum/qa-plugin/tips-widget/qa-plugin.php
@@ -0,0 +1,39 @@
+<?php
+/*
+  Plugin Name: Tips Widget
+  Plugin URI:
+  Plugin Description: Simple plugin for displaying random tip about forum as widget in sidebar
+  Plugin Version: 0.1
+  Plugin Date: 2017-11-05
+  Plugin Author: Patrycjerz
+  Plugin Author URI:
+  Plugin License: GPLv2
+  Plugin Minimum Question2Answer Version: 1.5
+  Plugin Update Check URI:
+*/
+
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+	header('Location: ../../');
+	exit;
+}
+
+qa_register_plugin_module(
+  'page',
+  'tips-widget-page.php',
+  'tips_widget_page',
+  'Tips Widget Page'
+);
+
+qa_register_plugin_module(
+  'widget',
+  'tips-widget-widget.php',
+  'tips_widget_widget',
+  'Tips Widget Widget'
+);
+
+qa_register_plugin_module(
+  'module',
+  'tips-widget-admin.php',
+  'tips_widget_admin',
+  'Tips Widget Admin'
+);
