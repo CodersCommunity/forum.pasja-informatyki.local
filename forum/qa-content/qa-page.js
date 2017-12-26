aa828d65f93a5a739ed3ee399b06e78b4f9b0c29
Krzysztof Pawelec <krzysztof.pawelec1@gmail.com>
2017-12-26 18:11:39 +0100

    Poprawki po CodeReview.

--- forum/qa-content/qa-page.js
+++ forum/qa-content/qa-page.js
@@ -873,81 +873,88 @@ function qa_ajax_error()
 /*
  * Feature: add @annotation with nick of user, to whom current comment refers to
  */
-;( function( document ) {
+;( function() {
 
     'use strict';
 
     window.addEventListener( 'DOMContentLoaded', () => {
-        document.querySelector( '.qa-main' ).addEventListener( 'click', function( ev ) {
-            const eTarget = ev.target;
+        const qaMainElement = document.querySelector( '.qa-main' );
 
-            if ( eTarget.value === 'skomentuj' || eTarget.value === 'odpowiedz' ) {
-                const matchedInstanceName = findCkeInstancePrefix( eTarget.name, eTarget.form.action );
-                const allCommentsForInstance = eTarget.form.querySelector( '[name="question-comments-list"]' ).children;
-                const allCommentsForInstanceLength = allCommentsForInstance.length;
-                const isResponseToLastComment = allCommentsForInstanceLength && allCommentsForInstance[ allCommentsForInstanceLength - 1 ].contains( eTarget );
+        if ( qaMainElement ) {
+            const addAnnotationToCommentedUser = ( relativeDomRef, ckeCurrentInstance ) => {
+                const chosenCommentAuthor = relativeDomRef.querySelector( '.vcard.author' ).textContent;
+                const ckeTxt = ckeCurrentInstance.document.$.querySelector( 'p' );
 
-                if ( matchedInstanceName === null || !allCommentsForInstanceLength || isResponseToLastComment ) {
+                let currentContent = '';
+
+                if ( ckeTxt.innerHTML ) {
+                    currentContent = ckeTxt.innerHTML.replace( /[<]strong[>](.*)[,<\/]strong[>]/, '' );
+                }
+
+                ckeTxt.innerHTML = `<strong>@${ chosenCommentAuthor },</strong> ${ currentContent }`;
+
+                return ckeTxt;
+            };
+
+            const setCursorToAnnotationEnd = ( editor, ckeTxt ) => {
+                editor.focus();
+
+                const currentRange = editor.getSelection().getRanges()[ 0 ];
+                const ckeNode = new CKEDITOR.dom.node( ckeTxt );
+                const newRange = new CKEDITOR.dom.range( currentRange.document );
+
+                newRange.moveToPosition( ckeNode, CKEDITOR.POSITION_BEFORE_END );
+                newRange.select();
+            };
+
+            const findCkeInstancePrefix = ( instanceSource, ancestorFormAction ) => {
+                const isAnswer = instanceSource === 'q_doanswer';
+
+                if ( isAnswer ) {
                     return;
                 }
 
-                const chosenCommentCKEInstance = Object.keys( CKEDITOR.instances ).find( ( instanceName ) => instanceName.includes( matchedInstanceName ) );
+                const isQuestionComment = instanceSource === 'q_docomment';
 
-                CKEDITOR.instances[ chosenCommentCKEInstance ].on( 'focus', ( ckeEvt ) => {
-                    handleCkeInstance( ckeEvt, chosenCommentCKEInstance, eTarget );
-                } );
-            }
-        } );
+                if ( isQuestionComment ) {
+                    return `c${ ancestorFormAction.split( '/' ).find( Number ) }`;
+                }
+                else {
+                    return `c${ instanceSource.slice( 1, instanceSource.indexOf( '_' ) ) }`;
+                }
+            };
 
-        const findCkeInstancePrefix = ( instanceSource, ancestorFormAction ) => {
-            const isAnswer = instanceSource === 'q_doanswer';
-
-            if ( isAnswer ) {
-                return null;
-            }
-
-            const isQuestionComment = instanceSource === 'q_docomment';
-
-            if ( isQuestionComment ) {
-                return `c${ ancestorFormAction.split( '/' ).find( ( urlPart ) => Number( urlPart ) ) }`;
-            } else {
-                return `c${ instanceSource.slice( 1, instanceSource.indexOf( '_' ) ) }`;
-            }
-        };
-
-        const handleCkeInstance = ( evt, chosenCommentCKEInstance, eTarget ) => {
-            setCursorToAnnotationEnd( evt.editor, addAnnotationToCommentedUser(
+            const handleCkeInstance = ( evt, chosenCommentCKEInstance, eTarget ) => {
+                setCursorToAnnotationEnd( evt.editor, addAnnotationToCommentedUser(
                     eTarget.parentNode.parentNode.parentNode, CKEDITOR.instances[ chosenCommentCKEInstance ]
-                )
-            );
+                    )
+                );
 
-            evt.removeListener();
-        };
+                evt.removeListener();
+            };
 
-        const addAnnotationToCommentedUser = ( relativeDomRef, ckeCurrentInstance ) => {
-            const chosenCommentAuthor = relativeDomRef.querySelector( '.vcard.author' ).textContent;
-            const ckeTxt = ckeCurrentInstance.document.$.querySelector( 'p' );
+            qaMainElement.addEventListener( 'click', ( ev ) => {
+                const eTarget = ev.target;
 
-            let currentContent = '';
+                if ( eTarget.value === 'skomentuj' || eTarget.value === 'odpowiedz' ) {
+                    const matchedInstanceName = findCkeInstancePrefix( eTarget.name, eTarget.form.action );
+                    const allCommentsForInstance = eTarget.form.querySelector( '[name="question-comments-list"]' ).children;
+                    const allCommentsForInstanceLength = allCommentsForInstance.length;
+                    const isResponseToLastComment = allCommentsForInstanceLength && allCommentsForInstance[ allCommentsForInstanceLength - 1 ].contains( eTarget );
 
-            if ( ckeTxt.innerHTML ) {
-                currentContent = ckeTxt.innerHTML.replace( /[<]strong[>](.*)[,<\/]strong[>]/, '' );
-            }
+                    if ( !matchedInstanceName || !allCommentsForInstanceLength || isResponseToLastComment ) {
+                        return;
+                    }
 
-            ckeTxt.innerHTML = `<strong>@${ chosenCommentAuthor },</strong> ${ currentContent }`;
+                    const chosenCommentCKEInstance = Object.keys( CKEDITOR.instances ).find( ( instanceName ) => {
+                        return instanceName.includes( matchedInstanceName );
+                    } );
 
-            return ckeTxt;
-        };
-
-        const setCursorToAnnotationEnd = ( editor, ckeTxt ) => {
-            editor.focus();
-
-            const currentRange = editor.getSelection().getRanges()[ 0 ];
-            const ckeNode = new CKEDITOR.dom.node( ckeTxt );
-            const newRange = new CKEDITOR.dom.range( currentRange.document );
-
-            newRange.moveToPosition( ckeNode, CKEDITOR.POSITION_BEFORE_END );
-            newRange.select();
-        };
+                    CKEDITOR.instances[ chosenCommentCKEInstance ].on( 'focus', ( ckeEvt ) => {
+                        handleCkeInstance( ckeEvt, chosenCommentCKEInstance, eTarget );
+                    } );
+                }
+            } );
+        }
     } );
-} ( document ) );
+} () );
