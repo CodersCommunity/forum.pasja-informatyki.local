ea7591e974acb387d61409a8377c568879ba9883
Mariusz08 <nowygracz01@gmail.com>
2018-02-20 15:14:48 +0100

    Check tab size

--- forum/qa-theme/SnowFlat/js/selectLanguage.js
+++ forum/qa-theme/SnowFlat/js/selectLanguage.js
@@ -16,54 +16,54 @@ document.addEventListener("DOMContentLoaded", function(event) {
             if ( location.href.includes( '/ask' ) ) {
                 codeBlock.addEventListener( 'click', () => {
                     const firstCategory = $( '#category_1 option:selected' );
-				
+                       	
                     if(firstCategory.text() === 'Programowanie') {
                         const secondCategory = $( '#category_2 option:selected' );			
-					    const findCategory = categories.find( function(object) {
+                        const findCategory = categories.find( function(object) {
                             return object.category == secondCategory.text();
                         });
-					
-				        if(findCategory != undefined){
-					        CKEDITOR.config.syntaxhighlight_lang = findCategory.language;
-				        } else {
-						    CKEDITOR.config.syntaxhighlight_lang = 'plain';
-					    }
-			
+                            			
+                        if(findCategory != undefined){
+                            CKEDITOR.config.syntaxhighlight_lang = findCategory.language;
+                        } else { 
+                            CKEDITOR.config.syntaxhighlight_lang = 'plain';
+                        }
+	                 
                     }
-				
-			    });
-			
-		    } else if ( location.href.includes( 'edit' ) ) {
+                     		
+                });
+	         	
+            } else if ( location.href.includes( 'edit' ) ) {
                 codeBlock.addEventListener( 'click', () => {
-					
+	               			
                     const questionEditFirstCategory = $( '#q_category_1 option:selected' );
                     if(questionEditFirstCategory.text() === 'Programowanie'){
                         const questionEditSecondCategory = $( '#q_category_2 option:selected' );
-                	    const findCategory = categories.find( function(object) {
+                        const findCategory = categories.find( function(object) {
                             return object.category == questionEditSecondCategory.text();
                         });
-			
-        			    if(findCategory != undefined){
-				            CKEDITOR.config.syntaxhighlight_lang = findCategory.language;
-				        } else {
-				            CKEDITOR.config.syntaxhighlight_lang = 'plain';
-				        }
+		            
+                        if(findCategory != undefined){
+                            CKEDITOR.config.syntaxhighlight_lang = findCategory.language;
+                        } else {
+                            CKEDITOR.config.syntaxhighlight_lang = 'plain';
+                        }
                     }
                 });
-				
-	        } else {
+	        		
+	    } else {
                 codeBlock.addEventListener( 'click', () => {
 				
                     const category = $( '.qa-q-view-where-data' );
-			        const findCategory = categories.find( function(object) {
+                    const findCategory = categories.find( function(object) {
                         return object.category == category.text();
                     });
-				
-				    if(findCategory != undefined){
-				        CKEDITOR.config.syntaxhighlight_lang = findCategory.language;
-				    } else {
-					    CKEDITOR.config.syntaxhighlight_lang = 'plain';
-				    }
+		     	
+                    if(findCategory != undefined){
+                        CKEDITOR.config.syntaxhighlight_lang = findCategory.language;
+                    } else {
+                        CKEDITOR.config.syntaxhighlight_lang = 'plain';
+                    }
 				
                 });
             }		   
