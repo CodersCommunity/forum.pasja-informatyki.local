46c91514accc98871953a6b0b90eca3b512069ca
Mariusz08 <nowygracz01@gmail.com>
2018-02-20 14:46:50 +0100

    Delete switch

--- forum/qa-theme/SnowFlat/js/selectLanguage.js
+++ forum/qa-theme/SnowFlat/js/selectLanguage.js
@@ -1,111 +1,72 @@
-if (typeof CKEDITOR != 'undefined') {
-    CKEDITOR.on( "instanceReady", function() {
-        const codeBlock = document.getElementById( "cke_43" );
-        if ( location.href.includes( "/ask" ) ) {
-            codeBlock.addEventListener( "click", function() {
-                const category_1 = $('#category_1 option:selected');
-                if(category_1.text() === 'Programowanie') {
-                    const category_2 = $('#category_2 option:selected');			
-                    switch(category_2.text()){
-                        case 'HTML i CSS':
-                            CKEDITOR.config.syntaxhighlight_lang = 'xml';
-                        break;
-                        case 'C i C++':
-                            CKEDITOR.config.syntaxhighlight_lang = 'cpp';
-                        break;
-                        case 'JavaScript, jQuery, AJAX':
-                            CKEDITOR.config.syntaxhighlight_lang = 'jscript';
-                        break;
-                        case 'PHP, Symfony, Zend':
-                            CKEDITOR.config.syntaxhighlight_lang = 'php';
-                        break;
-                        case 'SQL, bazy danych':
-                            CKEDITOR.config.syntaxhighlight_lang = 'sql';
-                        break;
-                        case 'C# i .NET':
-                            CKEDITOR.config.syntaxhighlight_lang = 'csharp';
-                        break;
-                        case 'Java':
-                            CKEDITOR.config.syntaxhighlight_lang = 'java';
-                        break;
-                        case 'Python, Django':
-                            CKEDITOR.config.syntaxhighlight_lang = 'python';
-                        break;
-                        default:
-                            CKEDITOR.config.syntaxhighlight_lang = 'plain';
-                        break;
-                    }    			
-                }    
-            });
-        } else if ( location.href.includes( "edit" ) ) {
-                codeBlock.addEventListener( "click", function() {
-                const questionEditCategory_1 = $('#q_category_1 option:selected');
-                if(questionEditCategory_1.text() === 'Programowanie'){
-                    const questionEditCategory_2 = $('#q_category_2 option:selected');
-                    switch(questionEditCategory_2.text()){
-                        case 'HTML i CSS':
-                            CKEDITOR.config.syntaxhighlight_lang = 'xml';
-                        break;
-                        case 'C i C++':
-                            CKEDITOR.config.syntaxhighlight_lang = 'cpp';
-                        break;
-                        case 'JavaScript, jQuery, AJAX':
-                            CKEDITOR.config.syntaxhighlight_lang = 'jscript';
-                        break;
-                        case 'PHP, Symfony, Zend':
-                            CKEDITOR.config.syntaxhighlight_lang = 'php';
-                        break;
-                        case 'SQL, bazy danych':
-                            CKEDITOR.config.syntaxhighlight_lang = 'sql';
-                        break;
-                        case 'C# i .NET':
-                            CKEDITOR.config.syntaxhighlight_lang = 'csharp';
-                        break;
-                        case 'Java':
-                            CKEDITOR.config.syntaxhighlight_lang = 'java';
-                        break;
-                        case 'Python, Django':
-                            CKEDITOR.config.syntaxhighlight_lang = 'python';
-                        break;
-                        default:
-                            CKEDITOR.config.syntaxhighlight_lang = 'plain';
-                        break;
-                    }   		
-                }
-            });
-	    } else {
-            codeBlock.addEventListener( 'click', function() {
-                const category = $('.qa-q-view-where-data').text();
-                switch(category){
-                    case 'HTML i CSS':
-                        CKEDITOR.config.syntaxhighlight_lang = 'xml';
-                    break;
-                    case 'C i C++':
-                        CKEDITOR.config.syntaxhighlight_lang = 'cpp';
-                    break;
-                    case 'JavaScript, jQuery, AJAX':
-                        CKEDITOR.config.syntaxhighlight_lang = 'jscript';
-                    break;
-                    case 'PHP, Symfony, Zend':
-                        CKEDITOR.config.syntaxhighlight_lang = 'php';
-                    break;
-                    case 'SQL, bazy danych':
-                        CKEDITOR.config.syntaxhighlight_lang = 'sql';
-                    break;
-                    case 'C# i .NET':
-                        CKEDITOR.config.syntaxhighlight_lang = 'csharp';
-                    break;
-                    case 'Java':
-                        CKEDITOR.config.syntaxhighlight_lang = 'java';
-                    break;
-                    case 'Python, Django':
-                        CKEDITOR.config.syntaxhighlight_lang = 'python';
-                    break;
-                    default:
-                        CKEDITOR.config.syntaxhighlight_lang = 'plain';
-                    break;
-                }   
-            });
-        }		   
-    });
+document.addEventListener("DOMContentLoaded", function(event) {
+    if (typeof CKEDITOR != 'undefined') {
+        CKEDITOR.on( 'instanceReady', function() {
+            const categories = [
+                {category : 'HTML i CSS', language: 'xml'},
+                {category : 'C i C++', language: 'cpp'},
+                {category : 'JavaScript, jQuery, AJAX', language: 'jscript'},
+                {category : 'PHP, Symfony, Zend', language: 'php'},
+                {category : 'SQL, bazy danych', language: 'sql'},
+                {category : 'C# i .NET', language: 'csharp'},
+                {category : 'Java', language: 'java'},
+                {category : 'Python, Django', language: 'python'},
+            ]
+            const codeBlock = document.getElementById( 'cke_43' );
+		
+            if ( location.href.includes( '/ask' ) ) {
+                codeBlock.addEventListener( 'click', () => {
+                    const firstCategory = $( '#category_1 option:selected' );
+				
+                    if(firstCategory.text() === 'Programowanie') {
+                        const secondCategory = $( '#category_2 option:selected' );			
+					    const findCategory = categories.find( function(object) {
+                            return object.category == secondCategory.text();
+                        });
+					
+				        if(findCategory != undefined){
+					        CKEDITOR.config.syntaxhighlight_lang = findCategory.language;
+				        } else {
+						    CKEDITOR.config.syntaxhighlight_lang = 'plain';
+					    }
+			
+                    }
+				
+			    });
+			
+		    } else if ( location.href.includes( 'edit' ) ) {
+                codeBlock.addEventListener( 'click', () => {
+					
+                    const questionEditFirstCategory = $( '#q_category_1 option:selected' );
+                    if(questionEditFirstCategory.text() === 'Programowanie'){
+                        const questionEditSecondCategory = $( '#q_category_2 option:selected' );
+                	    const findCategory = categories.find( function(object) {
+                            return object.category == questionEditSecondCategory.text();
+                        });
+			
+        			    if(findCategory != undefined){
+				            CKEDITOR.config.syntaxhighlight_lang = findCategory.language;
+				        } else {
+				            CKEDITOR.config.syntaxhighlight_lang = 'plain';
+				        }
+                    }
+                });
+				
+	        } else {
+                codeBlock.addEventListener( 'click', () => {
+				
+                    const category = $( '.qa-q-view-where-data' );
+			        const findCategory = categories.find( function(object) {
+                        return object.category == category.text();
+                    });
+				
+				    if(findCategory != undefined){
+				        CKEDITOR.config.syntaxhighlight_lang = findCategory.language;
+				    } else {
+					    CKEDITOR.config.syntaxhighlight_lang = 'plain';
+				    }
+				
+                });
+            }		   
+        });
+    }
 }
