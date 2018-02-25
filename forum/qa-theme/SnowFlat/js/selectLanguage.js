d218de47dff74054e6d0685f41f26a925081764f
Mariusz08 <nowygracz01@gmail.com>
2018-02-25 09:11:18 +0100

    Block code propose language when editing a comment.

--- forum/qa-theme/SnowFlat/js/selectLanguage.js
+++ forum/qa-theme/SnowFlat/js/selectLanguage.js
@@ -31,22 +31,28 @@ document.addEventListener("DOMContentLoaded", () => {
                      		
                 };
 	         	
-            } else if ( location.href.includes( 'edit' ) ) {
+           } else if ( location.href.includes( 'edit' ) ) {
                 clickHandler = () => {
 	               			
                     const firstCategory = document.querySelector( '#q_category_1' );
-                    const selectedFirstOption = firstCategory.children[ firstCategory.selectedIndex ];                       	
-                    if(selectedFirstOption.textContent === 'Programowanie') {
-                        const secondCategory = document.querySelector( '#q_category_2' );
-                        const selectedSecondOption = secondCategory.children[ secondCategory.selectedIndex ];
-                        const findCategory = categories.find( function(object) {
-                            return object.category == selectedSecondOption.textContent;
+                    if(firstCategory != undefined)
+                    {
+                        const selectedFirstOption = firstCategory.children[ firstCategory.selectedIndex ];                       	
+                        if(selectedFirstOption.textContent === 'Programowanie') 
+                        {
+                            const secondCategory = document.querySelector( '#q_category_2' );
+                            const selectedSecondOption = secondCategory.children[ secondCategory.selectedIndex ];
+                            var findCategory = categories.find( function(object) {
+                                return object.category == selectedSecondOption.textContent;
+                            });
+                        }
+                    } else {
+                       const category = document.querySelector( '.qa-q-view-where-data' );
+                       var findCategory = categories.find( function(object) {
+                            return object.category == category.textContent;
                         });
-		            
-                            CKEDITOR.config.syntaxhighlight_lang = findCategory ? findCategory.language : 'plain';
-                    } else {		
-                            CKEDITOR.config.syntaxhighlight_lang = 'plain';			
                     }
+                    CKEDITOR.config.syntaxhighlight_lang = findCategory ? findCategory.language : 'plain';
                 };
 	        		
 	    } else {
