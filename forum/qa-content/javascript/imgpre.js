ea5efba107a461182656ee291c47ac77fb43e853
Mariusz08 <mariuszhg08@gmail.com>
2018-03-24 10:35:09 +0100

    Changed tabs to 4 spaces in javascript files

--- forum/qa-content/javascript/imgpre.js
+++ forum/qa-content/javascript/imgpre.js
@@ -1,32 +1,32 @@
 (function() {
 
-	var iconsFolderURL = '/qa-theme/SnowFlat/icons/';
-	var icons = [
-		'code1.png',
-		'net1.png',
-		'os1.png',
-		'eth1.png',
-		'ph1.png',
-		'key1.png',
-		'stu1.png',
-		'egz1.png',
-		'new1.png',
-		'tut1.png',
-		'for1.png',
-		'tea1.png',
-		'ruler1.png',
-		'note1.png',
-		'brush1.png',
-		'off1.png'
-	];
+    var iconsFolderURL = '/qa-theme/SnowFlat/icons/';
+    var icons = [
+        'code1.png',
+        'net1.png',
+        'os1.png',
+        'eth1.png',
+        'ph1.png',
+        'key1.png',
+        'stu1.png',
+        'egz1.png',
+        'new1.png',
+        'tut1.png',
+        'for1.png',
+        'tea1.png',
+        'ruler1.png',
+        'note1.png',
+        'brush1.png',
+        'off1.png'
+    ];
 
-	window.addEventListener('load', function() {
-		
-		icons.forEach(function(icon) {
-			var asyncImg = new Image();
-			asyncImg.src = iconsFolderURL + icon;
-		});
+    window.addEventListener('load', function() {
+        
+        icons.forEach(function(icon) {
+            var asyncImg = new Image();
+            asyncImg.src = iconsFolderURL + icon;
+        });
 
-	});
-	
+    });
+    
 })();
