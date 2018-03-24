c020b7c0666827a06c9369b2dccdaad315a1feda
Mariusz08 <nowygracz01@gmail.com>
2018-03-24 12:24:16 +0100

    Changed tabs to 4 spaces in qa-tests folder

--- forum/index.php
+++ forum/index.php
@@ -22,9 +22,9 @@
 
 //	Set base path here so this works with symbolic links for multiple installations
 
-	define('QA_BASE_DIR', dirname(empty($_SERVER['SCRIPT_FILENAME']) ? __FILE__ : $_SERVER['SCRIPT_FILENAME']).'/');
+    define('QA_BASE_DIR', dirname(empty($_SERVER['SCRIPT_FILENAME']) ? __FILE__ : $_SERVER['SCRIPT_FILENAME']).'/');
 
-	require 'qa-include/qa-index.php';
+    require 'qa-include/qa-index.php';
 
 
 /*
