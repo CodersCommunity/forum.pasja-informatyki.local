04dddb969ea0b21943183c64af9737807b0c44b1
Mariusz08 <nowygracz01@gmail.com>
2018-04-10 17:46:58 +0200

    Update script.js

--- forum/qa-plugin/report-reason/script.js
+++ forum/qa-plugin/report-reason/script.js
@@ -1,32 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
     
     const flagboxPopup = document.querySelector("#flagbox-popup");
-    const erorrPopupHtml = document.querySelector('.html-error');
     const flagButton = document.querySelector('.qa-form-light-button-flag');
     flagButton.type = "button";
+    const wrap = document.querySelector('.qa-flag-reasons-wrap');
 
+    function showPopup() {
+        flagboxPopup.classList.remove("hide");
+        flagboxPopup.classList.add("show");
+    }
+    function hidePopup() {
+        flagboxPopup.classList.remove("show");
+        flagboxPopup.classList.add("hide");
+    }
+
+    wrap.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+
+    });
 
     flagButton.addEventListener("click", () => {
+        showPopup();    
 
-            const { postid: postId, posttype: postType, parentid: parentId } = flagButton.dataset;
-	    flagboxPopup.classList.remove("hide");
-            flagboxPopup.classList.add("show");
-            const closer = document.querySelector('.closer');
-            closer.addEventListener("click", () => {
-	        flagboxPopup.classList.remove("show");
-                flagboxPopup.classList.add("hide");
-            });
+        flagboxPopup.addEventListener("click", () => {
+            hidePopup();
+        });
 
-            const sendButton = document.querySelector('.qa-go-flag-send-button');
-            sendButton.addEventListener("click", () => {
+	const { postid: postId, posttype: postType, parentid: parentId } = flagButton.dataset;
+	    
+        const closer = document.querySelector('.closer');
+        closer.addEventListener("click", () => {
+	    hidePopup();
+        });
 
-            const flagReason = document.querySelector("input[name=qa-spam-reason-radio]:checked").value;
-            const flagNotice = document.querySelector(".qa-spam-reason-text").value;
+        const sendButton = document.querySelector('.qa-go-flag-send-button');
+        sendButton.addEventListener("click", () => {
 
-            const dataArray = {questionid: flagQuestionid, postid: postId, posttype: postType, reasonid: flagReason, notice: flagNotice};
-            const sendData = JSON.stringify(dataArray);
-            const errorMessage = 'Blad serwera. Prosze sprobowac za jakis czas';
-            let errorText = '';
+        const flagReason = document.querySelector("input[name=qa-spam-reason-radio]:checked");
+        const flagNotice = document.querySelector(".qa-spam-reason-text").value;
+
+        const dataArray = {questionid: flagQuestionid, postid: postId, posttype: postType, reasonid: flagReason, notice: flagNotice};
+        const sendData = JSON.stringify(dataArray);
+        const errorMessage = 'Blad serwera. Prosze sprobowac za jakis czas';
+        let errorText = '';
+
+        if(flagReason) {
 
             $.ajax({
                  type: "POST",
@@ -36,31 +55,35 @@ document.addEventListener("DOMContentLoaded", () => {
                  cache: false,
                  success: function(data)
                  {
-                    if(data.error)
-                    {
-                        errorText = errorMessage;
-                    }
-                    else if(data.success)
-                    {
-                        location.reload();
-                    }
-                    else
-                    {
-			            errorText = errorMessage;
-                    }
+                     if(data.error)
+                     {
+                         errorText = errorMessage;
+                     }
+                     else if(data.success)
+                     {
+                         location.reload();
+                     }
+                     else
+                     {
+	                 errorText = errorMessage;
+                     }
                  },
                  error: function(data)
                  {
-		             errorText = errorMessage;
+	             errorText = errorMessage; 
                  }
-            });
-	
+            });	
+        } else {
+            errorText = "Proszę wybrać powód zgłoszenia!";    
+        }
         if(errorText)
-	    {
-		    const errorHtml = document.createElement('p');
-		    errorHtml.innerHTML = errorText;
-		    errorPopupHtml.appendChild(errorHtml);
-	    }
+        {
+            const errorPopupHtml = document.querySelector('.html-error');
+	    const errorHtml = document.createElement('p');
+            errorHtml.innerHTML = errorText;
+	    errorPopupHtml.appendChild(errorHtml);
+	}
+            
         });
 
     });
