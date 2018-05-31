e2345790231e4fa79f68c445e3266bfa3e4e2482
Marek Woś <mwos@getresponse.com>
2018-05-31 22:31:07 +0200

    User account settings - add configure queries - setup a simple mechanism to on/off 2fa (in progress) - little refactor - add date in polish format when 2fa is enabled (currently without log this value in db)

--- forum/qa-plugin/q2a-googleauthenticator-login/q2a-googleauthenticator-layer.php
+++ forum/qa-plugin/q2a-googleauthenticator-login/q2a-googleauthenticator-layer.php
@@ -6,47 +6,107 @@ class qa_html_theme_layer extends qa_html_theme_base
     {
         parent::doctype();
 
-        $content = [];
-
-        if (qa_clicked('general_save_button')) {
-        }
-
-            if ('account' === $this->request && true === (bool) qa_opt('googleauthenticator_login')) {
+        if ('account' === $this->request && true === (bool) qa_opt('googleauthenticator_login')) {
             $content = [
                 'tags'    => 'method="post" action="' . qa_self_html() . '"',
                 'style'   => 'wide',
                 'title'   => qa_lang_html('plugin_2fa/title')
-                ];
+            ];
 
             $content += $this->enablePlugin();
+
+            $this->content['form_2fa'] = $content;
+            qa_html_theme_base::doctype();
+        }
+    }
+
+    private function getUserQuery($userid)
+    {
+        // Return the user with the specified userid (should return one user or null)
+        $users = qa_db_read_all_assoc(
+            qa_db_query_sub(
+                'SELECT  us.userid, us.2fa_enabled, us.email, us.handle, up.points FROM ^users us LEFT JOIN ^userpoints up ON us.userid = up.userid WHERE us.userid=$',
+                $userid
+            )
+        );
+
+        return empty($users) ? null : $users[0];
+    }
+
+    /**
+     * This method enables 2fa on selected user.
+     *
+     * @param $userid
+     * @param $isEnabled
+     *
+     * @return mixed
+     */
+    private function updateUserEnable2FA($userid, $isEnabled)
+    {
+        $result = qa_db_query_sub(
+            'UPDATE ^users SET 2fa_enabled=# WHERE userid=#',
+            $isEnabled,
+            $userid
+        );
+
+        if (true === $result) {
+            return $isEnabled;
         }
 
-        $this->content['form_2fa'] = $content;
-
-        qa_html_theme_base::doctype();
+        user_error('Nie udało się włączyć autoryzacji dwuetapowej. Zgłoś ten problem administratorowi.');
     }
 
     private function enablePlugin()
     {
-        $userActive2fa = false;
-        if ($userActive2fa) {
-            return [
-                'fields'  => [
-                    'old' => [
-                        'label' => qa_lang_html('plugin_2fa/plugin_is_enabled'),
-                        'tags'  => 'name="oldpassword" disabled',
-                        'value' => 'piątek, 21 marca 2018, 16:18',
-                        'type'  => 'input'
-                    ]
-                ],
-                'buttons' => [
-                    'enable' => [
-                        'label' => qa_lang_html('plugin_2fa/enable_plugin')
-                    ]
-                ]
-            ];
+        $useraccount   = $this->getUser();
+        $userActive2fa = $useraccount['2fa_enabled'];
+
+        if (qa_clicked('doenable2fa')) {
+            $userActive2fa = $this->updateUserEnable2FA($useraccount['userid'], true);
+            var_dump($userActive2fa);
+        } elseif (qa_clicked('dodisable2fa')) {
+            $userActive2fa = $this->updateUserEnable2FA($useraccount['userid'], false);
+            var_dump($userActive2fa);
         }
 
+        if (true === (bool) $userActive2fa) {
+            $result = $this->render2FAEnabledForm();
+        } else {
+            $result = $this->render2FADisabledForm();
+        }
+
+        return $result;
+    }
+
+    private function render2FAEnabledForm()
+    {
+        $time = new DateTime('now');
+        $formatter = new IntlDateFormatter('pl_PL', IntlDateFormatter::SHORT, IntlDateFormatter::SHORT);
+        $formatter->setPattern('EEEE, dd MMMM yyyy, hh:mm:ss');
+
+        return [
+            'fields'  => [
+                'old' => [
+                    'label' => qa_lang_html('plugin_2fa/plugin_is_enabled'),
+                    'tags'  => 'name="oldpassword" disabled',
+                    'value' => $formatter->format($time),
+                    'type'  => 'input'
+                ]
+            ],
+            'buttons' => [
+                'enable' => [
+                    'label' => qa_lang_html('plugin_2fa/enable_plugin')
+                ]
+            ],
+            'hidden'  => [
+                'dodisable2fa' => '1',
+                'code'         => qa_get_form_security_code('2faform')
+            ]
+        ];
+    }
+
+    private function render2FADisabledForm()
+    {
         return [
             'fields'  => [
                 'old' => [
@@ -56,11 +116,31 @@ class qa_html_theme_layer extends qa_html_theme_base
                 ]
             ],
             'buttons' => [
-                'enable' => [
+                'enable_2fa' => [
                     'label' => qa_lang_html('plugin_2fa/enable_plugin')
                 ]
+            ],
+            'hidden'  => [
+                'doenable2fa' => '1',
+                'code'        => qa_get_form_security_code('2faform')
             ]
         ];
+    }
+
+    /**
+     * @return null
+     */
+    private function getUser()
+    {
+        $userid = qa_get_logged_in_userid();
+
+        if (!isset($userid)) {
+            qa_redirect('login');
+        }
+
+        $useraccount = $this->getUserQuery($userid);
+
+        return $useraccount;
     }
 }
 
