9b0f7a4108b64ba0d4adff32ecf6a9564e81da7f
Marek Woś <marek.wos@getresponse.com>
2018-08-29 22:24:18 +0200

    Add sentry integration

--- forum/index.php
+++ forum/index.php
@@ -20,12 +20,22 @@
 	More about this license: http://www.question2answer.org/license.php
 */
 
+require_once 'vendor/sentry/sentry/lib/Raven/Autoloader.php';
 //	Set base path here so this works with symbolic links for multiple installations
+Raven_Autoloader::register();
+$client = new Raven_Client('https://2a54544850e24243abfa7afc0b22c79a@sentry.io/1271245');
+$error_handler = new Raven_ErrorHandler($client);
+$error_handler->registerExceptionHandler();
+$error_handler->registerErrorHandler();
+$error_handler->registerShutdownFunction();
 
-	define('QA_BASE_DIR', dirname(empty($_SERVER['SCRIPT_FILENAME']) ? __FILE__ : $_SERVER['SCRIPT_FILENAME']).'/');
+try {
+    define('QA_BASE_DIR', dirname(empty($_SERVER['SCRIPT_FILENAME']) ? __FILE__ : $_SERVER['SCRIPT_FILENAME']) . '/');
 
-	require 'qa-include/qa-index.php';
-
+    require 'qa-include/qa-index.php';
+} catch (\Exception $exception) {
+    $error_handler->handleException($exception);
+}
 
 /*
 	Omit PHP closing tag to help avoid accidental output
