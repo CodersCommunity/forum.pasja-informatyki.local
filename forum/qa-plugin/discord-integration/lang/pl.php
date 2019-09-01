bcdff6a764fefab867390f2ec4a3298bcd2c929e
Arkadiusz Waluk <arkadiusz@waluk.pl>
2019-09-01 12:21:50 +0200

    Integracja Discord (#161)
    * Add Discord integration plugin
    
    * Update Discord page URI
    
    * Change IRC to Discord in menu and tips widget
    
    * fix redirect uri
    
    * Remove user from Discord after ban
    
    * Update success message

---
+++ forum/qa-plugin/discord-integration/lang/pl.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'title' => 'Discord',
+    'join' => 'Dołącz do serwera',
+    'join_error' => 'Błąd dołączania! Spróbuj ponownie lub ^1skontaktuj się z nami^2 jeśli problem się powtórzy.',
+    'not_logged' => '^1Zaloguj się^2 lub ^3zarejestruj^4, aby dołączyć do Discorda',
+    'already_joined' => 'Jesteś już na naszym serwerze jako',
+    'success_joined' => 'Gratulacje, zostałeś dołączony na serwer! Teraz wystarczy, że włączysz Discorda, wybierzesz nasz serwer i możesz rozmawiać.',
+    'disconnect_button' => 'Odłącz powiązanie i opuść serwer',
+    'disconnected_success' => 'Twoje konto zostało pomyślnie odłączone.',
+    'user_blocked' => 'Twoje konto jest zablokowane - nie możesz dołączyć do Discorda.',
+    'already_connected_account' => 'To konto Discord jest już połączone z innym użytkownikiem.',
+
+    'client_id' => 'ID klienta API Discord',
+    'client_secret' => 'Sekretny klucz klienta API Discord',
+    'guild_id' => 'ID serwera Discord do dołączania',
+    'bot_token' => 'Token bota',
+    'top_info' => 'Informacja na górze strony (HTML dozwolony)',
+    'bottom_info' => 'Informacja na dole strony (HTML dozwolony)',
+    'admin_save_button' => 'Zapisz',
+    'admin_ok_info' => 'Zapisano!'
+];
