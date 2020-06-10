215d6f18147aa14b51f8994e71f99bb2915c73b4
Krzysztof Pawelec <krzysztof.pawelec1@gmail.com>
2020-06-10 02:41:54 +0200

    Refactor getPreElemsWithNotAvailableBrush function; Fix incorrectly differentiating unknown lang elements from not loaded ones

--- forum/qa-plugin/ckeditor4/plugins/syntaxhighlight/init.js
+++ forum/qa-plugin/ckeditor4/plugins/syntaxhighlight/init.js
@@ -83,7 +83,8 @@
             }));
         const preElemsWithUnknownLang = getPreElemsWithNotAvailableBrush();
 
-        const notFetchedBrushScripts = fetchNeededLangs(preElemsWithUnknownLang.notLoadedLangElems)
+        const notFetchedBrushScripts = fetchNeededLangs(preElemsWithUnknownLang.notLoadedLangElems);
+        console.warn('preElemsWithUnknownLang.unknownLangElems: ', preElemsWithUnknownLang.unknownLangElems, ' /preElemsWithUnknownLang.notLoadedLangElems: ', preElemsWithUnknownLang.notLoadedLangElems);
         fixUnavailableLangCodes([...preElemsWithUnknownLang.unknownLangElems, ...notFetchedBrushScripts]);
 
         function getPreElemsWithNotAvailableBrush() {
@@ -93,55 +94,67 @@
             const unknownLangElems = [];
             const notLoadedLangElems = [];
 
-            for (let i = 0; i < preElements.length; i++) {
-                const pre = preElements[i];
-
-                if (checkedUnknownLangNames.includes(pre.className)) {
-                    unknownLangElems.push(pre);
-                    continue;
-                }
-
-                if (checkedNotLoadedLangNames.includes(pre.className)) {
-                    notLoadedLangElems.push(pre);
-                    continue;
-                }
-
-                // const matchedBrush = loadedBrushes.find(() => {});
-
-                for (let j = 0; j < loadedBrushes.length; j++) {
-                    const { name, aliases } = loadedBrushes[j];
-
-                    const foundAlias = aliases
-                        .find(alias => SyntaxHighlighter.languages.codes.includes(alias));
-
-                    if (foundAlias) {
-                        checkedNotLoadedLangNames.push(pre.className);
-                        notLoadedLangElems.push(pre);
-
-                        break;
-                    }
-
-                    const regExp = new RegExp(`brush\:(${ aliases.join('|') });`);
-
-                    if (regExp.test(pre.className)) {
-                        checkedUnknownLangNames.push(pre.className);
-                        unknownLangElems.push(pre);
-
-                        break;
-                    }
-                }
-            }
+            scanPreElements();
 
             return {
                 'notLoadedLangElems': notLoadedLangElems,
                 'unknownLangElems': unknownLangElems
             };
+
+            function scanPreElements() {
+                for (let i = 0; i < preElements.length; i++) {
+                    const pre = preElements[i];
+                    const preClassName = pre.className;
+
+                    if (checkedNotLoadedLangNames.includes(preClassName)) {
+                        notLoadedLangElems.push(SyntaxHighlighter.defaults['code-language'].ctorName);
+                        continue;
+                    } else if (checkedUnknownLangNames.includes(preClassName)) {
+                        unknownLangElems.push(pre);
+                        continue;
+                    }
+
+                    matchPreElemsWithBrushes(pre, preClassName);
+                }
+            }
+
+            function matchPreElemsWithBrushes(pre, preClassName) {
+                let isBrashKnown = false;
+
+                for (let j = 0; j < loadedBrushes.length; j++) {
+                    const { name, aliases } = loadedBrushes[j];
+
+                    if (getBrushRegExp(aliases.join('|')).test(preClassName)) {
+                        isBrashKnown = true;
+                    } else {
+                        continue;
+                    }
+
+                    const foundAlias = aliases.find(alias => SyntaxHighlighter.languages.codes.includes(alias));
+
+                    if (foundAlias) {
+                        checkedNotLoadedLangNames.push(preClassName);
+                        notLoadedLangElems.push(name);
+
+                        break;
+                    }
+                }
+
+                if (!isBrashKnown) {
+                    checkedUnknownLangNames.push(preClassName);
+                    unknownLangElems.push(pre);
+                }
+            }
+
+            function getBrushRegExp(langName) {
+                return new RegExp(`brush\:(${ langName });`);
+            }
         }
 
         function fetchNeededLangs(neededBrushes) {
             const brushScripts = document.querySelectorAll('[src*="shBrush"]');
             const lastBrushScript = brushScripts[brushScripts.length - 1];
-            const brushSrcPrefix = lastBrushScript.src
+            const brushSrcPrefix = lastBrushScript.getAttribute('src')
                 .split('/')
                 .slice(0, -1)
                 .join('/');
@@ -155,6 +168,7 @@
                 const brushScript = document.createElement('script');
                 brushScript.async = false;
                 brushScript.addEventListener('error', onBrushLoadError, { once: true });
+                brushScript.type = 'text/javascript';
                 brushScript.src = `${ brushSrcPrefix }/shBrush${ ctorName }.js`;
 
                 lastBrushScript.parentNode.insertBefore(brushScript, lastBrushScript);
