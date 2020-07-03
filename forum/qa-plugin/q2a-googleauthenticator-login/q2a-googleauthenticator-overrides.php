3be702182eaff30e405dd9ea0b875137885871a8
Marek Woś <marwo12@gmail.com>
2020-07-03 20:09:23 +0200

    Drobne poprawki

--- forum/qa-plugin/q2a-googleauthenticator-login/q2a-googleauthenticator-overrides.php
+++ forum/qa-plugin/q2a-googleauthenticator-login/q2a-googleauthenticator-overrides.php
@@ -4,46 +4,37 @@ function qa_set_logged_in_user($userId, $handle = '', $remember = false, $source
 {
     require_once QA_INCLUDE_DIR . 'app/cookies.php';
     qa_start_session();
+
     if (!isset($userId)) {
-        // logout
-        qa_report_event(
-            'u_logout',
-            qa_get_logged_in_userid(),
-            qa_get_logged_in_handle(),
-            qa_cookie_get()
-        );
-        qa_clear_session_cookie();
-        qa_clear_session_user();
+        logout();
 
         return;
     }
 
-    // login
-    require_once QA_INCLUDE_DIR . 'db/selects.php';
-    $result = qa_db_read_all_assoc(
-        qa_db_query_sub(
-            'SELECT 2fa_enabled FROM ^users WHERE userid = #', $userId
-        )
-    );
-    if (count($result) !== 1) {
-        echo 'Invalid num_rows';
-        die;
-    }
-    $usingTwoFactorAuth = (bool) $result[0]['2fa_enabled'];
+    $usingTwoFactorAuth = userHasTwoFactorAuthentication($userId);
     if ($usingTwoFactorAuth && '2fa' !== $source) {
         echo '
-<form method="post" id="form" action="./2fa-auth">
+<form method="post" id="2fa-auth-form" action="./2fa-auth">
 	<input name="login" type="hidden" value="' . qa_post_text('emailhandle') . '">
 	<input name="password" type="hidden" value="' . qa_post_text('password') . '">
 	<input name="remember" type="hidden" value="' . qa_post_text('remember') . '">
 	<input name="redirect" type="hidden" value="' . $_GET['to'] . '">
 </form>
 <script>
-document.getElementById("form").submit();
+document.getElementById("2fa-auth-form").submit();
 </script>
 ';
         exit;
     }
+    [$userInfo, $sessionCode] = setLoggedUserInDb($userId, $source);
+
+    qa_db_user_logged_in($userId, qa_remote_ip_address());
+    qa_set_session_cookie($handle, $sessionCode, $remember);
+    qa_report_event('u_login', $userId, $userInfo['handle'], qa_cookie_get());
+}
+
+function setLoggedUserInDb($userId, $source): array
+{
     $userInfo = qa_db_single_select(
         qa_db_user_account_selectspec(
             $userId,
@@ -57,12 +48,41 @@ document.getElementById("form").submit();
             [
                 'sessioncode'   => $sessionCode,
                 'sessionsource' => $source
-            ]
+            ], ''
         );
     } else {
         $sessionCode = $userInfo['sessioncode'];
     }
-    qa_db_user_logged_in($userId, qa_remote_ip_address());
-    qa_set_session_cookie($handle, $sessionCode, $remember);
-    qa_report_event('u_login', $userId, $userInfo['handle'], qa_cookie_get());
+
+    return [$userInfo, $sessionCode];
+}
+
+function userHasTwoFactorAuthentication($userId): bool
+{
+    require_once QA_INCLUDE_DIR . 'db/selects.php';
+    $result = qa_db_read_all_assoc(
+        qa_db_query_sub(
+            'SELECT 2fa_enabled FROM ^users WHERE userid = #',
+            $userId
+        )
+    );
+
+    if (count($result) !== 1) {
+        echo 'Invalid num_rows';
+        die;
+    }
+
+    return (bool) $result[0]['2fa_enabled'];
+}
+
+function logout(): void
+{
+    qa_report_event(
+        'u_logout',
+        qa_get_logged_in_userid(),
+        qa_get_logged_in_handle(),
+        qa_cookie_get()
+    );
+    qa_clear_session_cookie();
+    qa_clear_session_user();
 }
