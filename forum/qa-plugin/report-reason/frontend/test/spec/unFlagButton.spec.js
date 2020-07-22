1848f82071083408d198ca745f5b5954211bd76d
Krzysztof Pawelec <krzysztof.pawelec1@gmail.com>
2020-07-22 21:47:31 +0200

    Add remaining unit tests for unFlagButton.js file;

--- forum/qa-plugin/report-reason/frontend/test/spec/unFlagButton.spec.js
+++ forum/qa-plugin/report-reason/frontend/test/spec/unFlagButton.spec.js
@@ -1,16 +1,72 @@
-// global.FLAG_REASONS_METADATA = {};
-
-// const index = require('../../dist/script');
-// console.log('index: ', index);
-
 const { expect } = require('chai');
 const { default: getUnFlagButtonHTML } = require('../cjs-src/unFlagButton');
 
-// console.log('getUnFlagButtonHTML ???', getUnFlagButtonHTML);
+const defaultParams = Object.freeze({
+    questionId: 1,
+    postId: 2,
+    parentId: 3
+});
 
-describe('getUnFlagButtonHTML', () => {
+const normalizeHTML = html => html.replace(/\s{2,}|\t|\n|\r/g, '');
+
+describe('getUnFlagButtonHTML()', () => {
+    it('should return button for question postType', () => {
+        const expected =
+            `<input name="q_dounflag"
+                    onclick="qa_show_waiting_after(this, false)"
+                    value="wycofaj zgłoszenie"
+                    title="Wycofaj zgłoszenie tej treści"
+                    type="submit" 
+                    class="qa-form-light-button qa-form-light-button-unflag">`;
+
+        expect(normalizeHTML(getUnFlagButtonHTML({
+            ...defaultParams,
+            postType: 'q'
+        }))).to.equal(normalizeHTML(expected));
+    });
+
+    it('should return button for answer postType', () => {
+        const expected =
+            `<input name="a2_dounflag"
+                    onclick="return qa_answer_click(2, 1, this);"
+                    value="wycofaj zgłoszenie"
+                    title="Wycofaj zgłoszenie tej treści"
+                    type="submit" 
+                    class="qa-form-light-button qa-form-light-button-unflag">`;
+
+        expect(normalizeHTML(getUnFlagButtonHTML({
+            ...defaultParams,
+            postType: 'a'
+        }))).to.equal(normalizeHTML(expected));
+    });
+
+    it('should return button for comment postType', () => {
+        const expected =
+            `<input name="c2_dounflag"
+                    onclick="return qa_comment_click(2, 1, 3, this);"
+                    value="wycofaj zgłoszenie"
+                    title="Wycofaj zgłoszenie tej treści"
+                    type="submit" 
+                    class="qa-form-light-button qa-form-light-button-unflag">`;
+
+        expect(normalizeHTML(getUnFlagButtonHTML({
+            ...defaultParams,
+            postType: 'c'
+        }))).to.equal(normalizeHTML(expected));
+    });
+
+    it('should throw error when at least one parameter is missing', () => {
+        const expected = 'Cannot destructure property `postType` of \'undefined\' or \'null\'';
+
+        expect(getUnFlagButtonHTML).to.throw(Error, expected);
+    });
+
     it('should throw error when postType is not recognized', () => {
-       expect(getUnFlagButtonHTML.bind(null, {})).to.throw(Error, 'Unrecognized postType: undefined for questionId: undefined and postId: undefined');
-       // expect(typeof getUnFlagButtonHTML()).toBe('string');
+        const expected = 'Unrecognized postType: x for questionId: 1 and postId: 2';
+
+        expect(getUnFlagButtonHTML.bind(null, {
+            ...defaultParams,
+            postType: 'x'
+        })).to.throw(Error, expected);
     });
 });
