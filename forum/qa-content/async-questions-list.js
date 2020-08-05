7607fdd2256e5a1fc8cd1c4fa978b605bc4bb02a
Krzysztof Pawelec <krzysztof.pawelec1@gmail.com>
2020-08-05 14:17:05 +0200

    [WIP] Refactor async-questions-list.js file; Implement showing clickable notification about new posts, which will update the list;

--- forum/qa-content/async-questions-list.js
+++ forum/qa-content/async-questions-list.js
@@ -1,38 +1,144 @@
-window.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function runAsyncQuestionsList() {
+	const { pathname } = window.location;
+	const isMainOrActivityPage = /^$|\/$|(\/?)activity/.test(pathname);
 
-	// Is this the main page?
-	if ($('.qa-template-qa, .qa-template-activity').length === 0) {
-		return // no
+	if (!isMainOrActivityPage) {
+		return;
 	}
 
-	// connect to websocket
-	var socket = new WebSocket('ws://localhost:3000')
-	window._socket = socket;
+	const QA_Q_LIST_SELECTOR = '.qa-q-list';
+	const questionList = document.querySelector(QA_Q_LIST_SELECTOR);
+	const newContentNotifier = createNewContentNotifier();
+	const webSocket = setupWebSocketConnection();
 
+	function setupWebSocketConnection() {
+		const webSocket = new WebSocket('ws://localhost:3000');
 
-	// get question list node
-	var $questionList = $('.qa-q-list')
+		// TODO: just for tests
+		window._webSocket = webSocket;
 
-	socket.addEventListener('open', function(event) {
+		webSocket.addEventListener('open', onOpen);
+		webSocket.addEventListener('message', onMessage);
+		webSocket.addEventListener('error', onError);
+
+		window.addEventListener('beforeunload', () => {
+			console.warn('close webSocket...');
+
+			webSocket.close();
+			newContentNotifier.disconnectIntersectionObserver();
+		});
+
+		return webSocket;
+	}
+
+	function onOpen(event) {
 		console.warn('open: ', event);
-	});
 
-	// listen to message
-	socket.addEventListener('message', function(event) {
+		webSocket.send(JSON.stringify({ pathname }));
+	}
 
-		var data = window.JSON.parse(event.data)
+	function onMessage(event) {
+		const data = window.JSON.parse(event.data)
 		console.warn('data: ', data);
-		$questionList.html(data.minifiedQuestionList)
 
-	})
+		notifyUserAboutNewContent();
+	}
 
-	socket.addEventListener('error', event => {
+	function onError(event) {
 		console.error('Socket error: ', event);
-	})
 
-	window.addEventListener('beforeunload', () => {
-		socket.close();
-	});
+		// TODO: send error via Ajax to Node, which will send an email to PM
+	}
 
-	console.warn('...listen to socket msgs');
-})
+	function notifyUserAboutNewContent() {
+		newContentNotifier.show();
+	}
+
+	function createNewContentNotifier() {
+		const notifierTargetPlace = document.querySelector('.qa-part-q-list');
+		const NOTIFIER_CLASSES = Object.freeze({
+			BASE: 'qa-custom-new-content-notifier',
+			VISIBLE: 'qa-custom-new-content-notifier--visible'
+		});
+		const { notifier, notifierCounter } = createNotifierDOM();
+		const disconnectIntersectionObserver = observeNotifierIntersections(notifier);
+
+		let notifierCounterValue = 0;
+
+		////
+		show();
+
+		return { show, hide, disconnectIntersectionObserver };
+
+		function createNotifierDOM() {
+			const notifier = document.createElement('button');
+			notifier.title = 'Aktualizuj listę postów';
+			notifier.classList.add(NOTIFIER_CLASSES.BASE);
+			notifier.innerHTML = `
+				<span>Nowych postów: </span>
+				<output></output>
+			`;
+			notifier.addEventListener('click', updatePageContent);
+
+			notifierTargetPlace.insertAdjacentElement('beforebegin', notifier);
+
+			const [ , notifierCounter ] = notifier.children;
+
+			return { notifier, notifierCounter };
+		}
+
+		function show() {
+			notifier.classList.add(NOTIFIER_CLASSES.VISIBLE);
+			incrementCounter();
+		}
+
+		function hide() {
+			notifier.classList.remove(NOTIFIER_CLASSES.VISIBLE);
+			resetCounter();
+		}
+
+		function incrementCounter() {
+			notifierCounter.textContent = ++notifierCounterValue;
+		}
+
+		function resetCounter() {
+			notifierCounterValue = 0;
+			notifierCounter.textContent = notifierCounterValue;
+		}
+	}
+
+	function updatePageContent({ currentTarget }) {
+		fetch(window.location.href)
+			.then((response) => response.text())
+			.then((html) => {
+				const responseQuestionList =
+						new DOMParser()
+							.parseFromString(html, 'text/html')
+							.querySelector(QA_Q_LIST_SELECTOR);
+
+				showUpdatedPageContent(responseQuestionList.innerHTML, currentTarget);
+			});
+	}
+
+	function showUpdatedPageContent(updatedContent, notifier) {
+		// TODO: consider wrapping it with requestAnimationFrame function
+		questionList.innerHTML = updatedContent;
+		newContentNotifier.hide();
+
+		if (notifier.classList.contains('fixed-to-top')) {
+			window.scroll({
+				top: 0,
+				behavior: 'smooth'
+			});
+		}
+	}
+
+	function observeNotifierIntersections(target) {
+		const intersectionObserver = new IntersectionObserver(([entry]) => {
+			target.classList.toggle('fixed-to-top', entry.intersectionRatio <= 0);
+		});
+		intersectionObserver.observe(target.previousElementSibling);
+
+		return intersectionObserver.disconnect.bind(intersectionObserver);
+	}
+});
