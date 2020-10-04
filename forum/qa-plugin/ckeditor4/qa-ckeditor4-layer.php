e96424d08425eec68d211858b175c7ebbd91ddcf
Arkadiusz Waluk <arkadiusz@waluk.pl>
2020-10-04 13:04:20 +0200

    Refactor qa-ckeditor4-layer.php

--- forum/qa-plugin/ckeditor4/qa-ckeditor4-layer.php
+++ forum/qa-plugin/ckeditor4/qa-ckeditor4-layer.php
@@ -1,4 +1,5 @@
 <?php
+
 if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
     header('Location: ../../');
     exit;
@@ -10,28 +11,24 @@ class qa_html_theme_layer extends qa_html_theme_base
     {
         if (qa_opt('ckeditor4_select')) {
             $selectable = false;
-            if (qa_opt('editor_for_qs') == 'CKEditor4')
-                $CK4_for_qs = true;
-            else
-                $CK4_for_qs = false;
-            if (qa_opt('editor_for_as') == 'CKEditor4')
-                $CK4_for_as = true;
-            else
-                $CK4_for_as = false;
+            $CK4_for_qs = qa_opt('editor_for_qs') === 'CKEditor4';
+            $CK4_for_as = qa_opt('editor_for_as') === 'CKEditor4';
+
             if ($CK4_for_qs) {
-                if ($this->template == 'ask' || $this->template == 'question') {
+                if ($this->template === 'ask' || $this->template === 'question') {
                     if (strpos($key, 'form') === 0 || strpos($key, 'form_q_edit') === 0) {
                         $selectable = true;
                     }
                 }
             }
             if ($CK4_for_as) {
-                if ($this->template == 'question') {
+                if ($this->template === 'question') {
                     if (strpos($key, 'a_form') === 0 || strpos($key, 'form_a_edit') === 0) {
                         $selectable = true;
                     }
                 }
             }
+
             if ($selectable) {
                 if (isset($part['fields']['content'])) {
                     $basic_checked = '';
@@ -60,22 +57,23 @@ class qa_html_theme_layer extends qa_html_theme_base
                     $html .= '</span>';
 
                     $content = &$part['fields']['content'];
-                    if (isset($content['label']))
-                        $content['label'] = $html . $content['label'];
-                    else
-                        $content['label'] = $html;
+                    $content['label'] = $html;
+                    if (isset($content['label'])) {
+                        $content['label'] .= $content['label'];
+                    }
                 }
             }
         }
-        qa_html_theme_base::main_part($key, $part);
+
+        parent::main_part($key, $part);
     }
 
     public function head_css()
     {
-        $this->content['css_src'][] = '/qa-content/css/shCore.css';
-        $this->content['css_src'][] = '/qa-content/css/shThemeDefault.css';
+        $this->content['css_src'][] = '/qa-content/css/shCore.css?v=' . QA_RESOURCE_VERSION;
+        $this->content['css_src'][] = '/qa-content/css/shThemeDefault.css?v=' . QA_RESOURCE_VERSION;
 
-        qa_html_theme_base::head_css();
+        parent::head_css();
     }
 
     public function main()
