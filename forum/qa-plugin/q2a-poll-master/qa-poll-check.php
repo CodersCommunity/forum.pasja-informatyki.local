d315d1ef92a2e9156ef9c6132d02945e929efce7
Arkadiusz Waluk <arkadiusz@waluk.pl>
2020-11-07 12:07:32 +0100

    Ankieta nie zostanie utworzona gdy nie zostaną podane opcje (#234)
    * Refactor qa-poll-check.php
    
    * Move queries for prepare database to dedicated method
    
    * Disable creating poll when options not provided or options are empty

--- forum/qa-plugin/q2a-poll-master/qa-poll-check.php
+++ forum/qa-plugin/q2a-poll-master/qa-poll-check.php
@@ -1,83 +1,62 @@
 <?php
 
-	class qa_poll_event {
-		function process_event($event, $userid, $handle, $cookieid, $params) {
-			if (qa_opt('poll_enable')) {
-				switch ($event) {
-					case 'q_post':
-						if(qa_post_text('is_poll')) {
-							qa_db_query_sub(
-								'CREATE TABLE IF NOT EXISTS ^postmeta (
-								meta_id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-								post_id bigint(20) unsigned NOT NULL,
-								meta_key varchar(255) DEFAULT \'\',
-								meta_value longtext,
-								PRIMARY KEY (meta_id),
-								KEY post_id (post_id),
-								KEY meta_key (meta_key)
-								) ENGINE=MyISAM  DEFAULT CHARSET=utf8'
-							);			
-							qa_db_query_sub(
-								'CREATE TABLE IF NOT EXISTS ^polls (
-								id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-								parentid bigint(20) unsigned NOT NULL,
-								votes longtext,
-								content varchar(255) DEFAULT \'\',
-								PRIMARY KEY (id)
-								) ENGINE=MyISAM  DEFAULT CHARSET=utf8'
-							);			
-							qa_db_query_sub(
-								'INSERT INTO ^postmeta (post_id,meta_key,meta_value) VALUES (#,$,$)',
-								$params['postid'],'is_poll',(qa_post_text('poll_multiple')?'2':'1')
-							);
+class qa_poll_event
+{
+    public function process_event($event, $userid, $handle, $cookieid, $params)
+    {
+        if (qa_opt('poll_enable') && $event === 'q_post' && qa_post_text('is_poll') === '1') {
+            $number = 0;
+            $added = 0;
+            while (isset($_POST['poll_answer_' . (++$number)])) {
+                if (!qa_post_text('poll_answer_' . $number)) {
+                    continue;
+                }
 
-							$c = 0;
-							while(isset($_POST['poll_answer_'.(++$c)])) {
-								if(!qa_post_text('poll_answer_'.$c)) continue; // empty
-								qa_db_query_sub(
-									'INSERT INTO ^polls (parentid,content) VALUES (#,$)',
-									$params['postid'],qa_post_text('poll_answer_'.$c)
-								);								
-							}
-						}
-						break;
-					default:
-						break;
-				}
-			}
-		}
-	}
+                qa_db_query_sub(
+                    'INSERT INTO ^polls (parentid,content) VALUES (#,$)',
+                    $params['postid'],
+                    qa_post_text('poll_answer_' . $number)
+                );
+                $added++;
+            }
 
-/*
+            if ($added > 0) {
+                qa_db_query_sub(
+                    'INSERT INTO ^postmeta (post_id,meta_key,meta_value) VALUES (#,$,$)',
+                    $params['postid'],
+                    'is_poll',
+                    (qa_post_text('poll_multiple') ? '2' : '1')
+                );
+            }
+        }
+    }
 
-					// buddypress integration
-						if (qa_opt('buddypress_integration_enable')) {
-						
-							$parent = qa_db_single_select(qa_db_full_post_selectspec(null, $answer['parentid']));
-							
-							require_once QA_INCLUDE_DIR.'qa-app-users.php';
-							
-							$publictohandle=qa_get_public_from_userids(array($userid));
-							$handle=@$publictohandle[$userid];
+    public function init_queries($tableslc)
+    {
+        $queries = [];
 
-							$anchor = qa_anchor('A', $params['postid']);
-							$suffix = '<a href="'.qa_path_html(qa_q_request($parent['postid'], $parent['title']), null, qa_opt('site_url'),null,$anchor).'">'.$parent['title'].'</a>';
+        if (!in_array(qa_db_add_table_prefix('postmeta'), $tableslc)) {
+            $queries[] = 'CREATE TABLE IF NOT EXISTS ^postmeta (
+                meta_id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                post_id bigint(20) unsigned NOT NULL,
+                meta_key varchar(255) DEFAULT \'\',
+                meta_value longtext,
+                PRIMARY KEY (meta_id),
+                KEY post_id (post_id),
+                KEY meta_key (meta_key)
+            ) ENGINE=MyISAM  DEFAULT CHARSET=utf8';
+        }
 
-							$activity_url = qa_path_html(qa_q_request($parent['postid'], $parent['title']), null, qa_opt('site_url'));
-							
-							$action = '<a href="' . bp_core_get_user_domain($userid) . '" rel="nofollow">'.$handle.'</a> voted in the poll "'.$suffix.'"';
+        if (!in_array(qa_db_add_table_prefix('polls'), $tableslc)) {
+            $queries[] = 'CREATE TABLE IF NOT EXISTS ^polls (
+                id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                parentid bigint(20) unsigned NOT NULL,
+                votes longtext,
+                content varchar(255) DEFAULT \'\',
+                PRIMARY KEY (id)
+            ) ENGINE=MyISAM  DEFAULT CHARSET=utf8';
+        }
 
-							qa_buddypress_activity_post(
-								array(
-									'action' => $action,
-									'content' => null,
-									'primary_link' => $activity_url,
-									'component' => 'bp-qa',
-									'type' => 'activity_qa',
-									'user_id' => $userid,
-									'item_id' => null
-								)
-							);
-						}
-						
-*/					
+        return $queries;
+    }
+}
