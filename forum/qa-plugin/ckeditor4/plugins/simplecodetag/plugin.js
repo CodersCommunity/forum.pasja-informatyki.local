5cfdc7c8a7f11bffe1cd450707ecf0f026ddecca
Krzysztof Pawelec <ScriptyChris@gmail.com>
2020-12-31 15:32:04 +0100

    Insert highlighted text in <code> tags on clicking the button;

--- forum/qa-plugin/ckeditor4/plugins/simplecodetag/plugin.js
+++ forum/qa-plugin/ckeditor4/plugins/simplecodetag/plugin.js
@@ -1,27 +1,38 @@
 console.warn('[0] CKEDITOR plugin script!!!');
 
-CKEDITOR.plugins.add( 'simplecodetag', {
+CKEDITOR.plugins.add('simplecodetag', {
+  // TODO: add icon file
   icons: 'simpleCodeTag',
-  init: function( editor ) {
-    console.log('[1] cke simpleCodeTag init');
+  init(editor) {
+    console.warn('[1] cke simpleCodeTag init /editor:', editor);
 
-    editor.addCommand( 'insertSimpleCodeTag', {
-      exec: function( editor ) {
-        console.log('cke simpleCodeTag exec');
+    editor.addCommand('insertSimpleCodeTag', {
+      exec(editor) {
+        const selectedText = editor.getSelection().getSelectedText();
+        console.warn('[2] cke simpleCodeTag exec /selectedText:' ,selectedText);
 
-        editor.insertHtml( 'The simpleCodeTag is inserted!' );
-      }
+        const htmlElement = new CKEDITOR.dom.element( 'code');
+        // set content as text to avoid XSS
+        htmlElement.setText(selectedText);
+
+        editor.insertElement(htmlElement);
+      },
     });
-    editor.ui.addButton( 'SimpleCodeTag', {
+
+    editor.ui.addButton('SimpleCodeTag', {
       label: 'Insert Code Tag',
       command: 'insertSimpleCodeTag',
-      // toolbar: 'links'
     });
   }
 });
 
-//CKEDITOR.config.extraPlugins += ',simplecodetag'
+/* CSS
 
-// do this in page inline script inside after `qa_ckeditor4_config` object creation
-//qa_ckeditor4_config.toolbar[7].push('SimpleCodeTag');
-//qa_ckeditor4_config.extraPlugins += ',simplecodetag';
+//color: #242729;
+color: rgb(255 255 255);
+//background-color: #e4e6e8;
+background-color: #2c3e50;
+padding: 2px 4px;
+border-radius: 2px;
+
+*/
