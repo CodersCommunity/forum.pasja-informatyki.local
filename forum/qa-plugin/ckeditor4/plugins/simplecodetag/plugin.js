4208f532ed0c7db3b0c72079a58f3ee8f86c7313
Krzysztof Pawelec <ScriptyChris@gmail.com>
2020-12-31 20:29:26 +0100

    Implement <code> element state toggling: wrap or unwrap, depending on whether text is already wrapped with it or not;

--- forum/qa-plugin/ckeditor4/plugins/simplecodetag/plugin.js
+++ forum/qa-plugin/ckeditor4/plugins/simplecodetag/plugin.js
@@ -2,33 +2,59 @@ CKEDITOR.plugins.add('simplecodetag', {
   // TODO: add icon file
   icons: 'simpleCodeTag',
   init(editor) {
-    editor.addCommand('insertSimpleCodeTag', {
+    const CODE_TAG_NAME = 'code';
+
+    editor.addCommand('toggleSimpleCodeTagInsertion', {
       exec(editor) {
-        const selectedText = editor.getSelection().getSelectedText();
-        const htmlElement = new CKEDITOR.dom.element( 'code');
+        const editorSelection = editor.getSelection();
+        const selectedElement =
+            editorSelection.getSelectedElement() || /* fallback as `.getSelectedElement()` might return null: https://dev.ckeditor.com/ticket/7928 */
+            editorSelection.getStartElement();
+        const isCodeElementInserted = selectedElement && selectedElement.$.tagName.toLowerCase() === CODE_TAG_NAME;
 
-        // set content as text to avoid XSS
-        htmlElement.setText(selectedText);
-
-        // use inline styling, because CKEditor is loaded inside iframe,
-        // so styles declared outside it (like qa-styles.css) cannot be applied
-        htmlElement.setStyles({
-          color: 'white',
-          background: '#2c3e50',
-          padding: '2px 4px',
-          borderRadius: '2px',
-        });
-
-        editor.insertElement(htmlElement);
+        if (isCodeElementInserted) {
+          unWrapTextFromCodeTag(selectedElement, editorSelection);
+        } else {
+          wrapTextInCodeTag(editorSelection);
+        }
       },
     });
 
     const CTRL_K = CKEDITOR.CTRL + 75;
-    editor.setKeystroke(CTRL_K, 'insertSimpleCodeTag');
+    editor.setKeystroke(CTRL_K, 'toggleSimpleCodeTagInsertion');
 
     editor.ui.addButton('SimpleCodeTag', {
       label: 'Insert Code Tag',
-      command: 'insertSimpleCodeTag',
+      command: 'toggleSimpleCodeTagInsertion',
     });
+
+    function unWrapTextFromCodeTag(selectedElement, editorSelection) {
+      const elementContent = new CKEDITOR.dom.element.get(selectedElement.$.childNodes[0]);
+
+      selectedElement.insertBeforeMe(elementContent);
+      selectedElement.remove();
+
+      editorSelection.selectElement(elementContent);
+    }
+
+    function wrapTextInCodeTag(editorSelection) {
+      const htmlElement = new CKEDITOR.dom.element(CODE_TAG_NAME);
+      const selectedText = editorSelection.getSelectedText() || /* if nothing is selected then prepare empty space */ ' ';
+
+      // set content as text to avoid XSS
+      htmlElement.setText(selectedText);
+
+      // use inline styling, because CKEditor is loaded inside iframe,
+      // so styles declared outside it (like qa-styles.css) cannot be applied
+      htmlElement.setStyles({
+        color: 'white',
+        background: '#2c3e50',
+        padding: '2px 4px',
+        borderRadius: '2px',
+      });
+
+      editor.editable().insertElementIntoSelection(htmlElement);
+      editorSelection.selectElement(htmlElement);
+    }
   }
 });
