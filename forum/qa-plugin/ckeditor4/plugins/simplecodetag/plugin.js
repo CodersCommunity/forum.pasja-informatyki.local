a5d564d232ba3c2bd1b505fd24472e840da937ba
Krzysztof Pawelec <ScriptyChris@gmail.com>
2020-12-31 16:08:12 +0100

    Style code tag element; Implement inserting tag on 'CTRL + K' keyboard shortcut;

--- forum/qa-plugin/ckeditor4/plugins/simplecodetag/plugin.js
+++ forum/qa-plugin/ckeditor4/plugins/simplecodetag/plugin.js
@@ -1,23 +1,30 @@
-console.warn('[0] CKEDITOR plugin script!!!');
-
 CKEDITOR.plugins.add('simplecodetag', {
   // TODO: add icon file
   icons: 'simpleCodeTag',
   init(editor) {
-    console.warn('[1] cke simpleCodeTag init /editor:', editor);
-
     editor.addCommand('insertSimpleCodeTag', {
       exec(editor) {
         const selectedText = editor.getSelection().getSelectedText();
-        console.warn('[2] cke simpleCodeTag exec /selectedText:' ,selectedText);
-
         const htmlElement = new CKEDITOR.dom.element( 'code');
+
         // set content as text to avoid XSS
         htmlElement.setText(selectedText);
+
+        // use inline styling, because CKEditor is loaded inside iframe,
+        // so styles declared outside it (like qa-styles.css) cannot be applied
+        htmlElement.setStyles({
+          color: 'white',
+          background: '#2c3e50',
+          padding: '2px 4px',
+          borderRadius: '2px',
+        });
 
         editor.insertElement(htmlElement);
       },
     });
+
+    const CTRL_K = CKEDITOR.CTRL + 75;
+    editor.setKeystroke(CTRL_K, 'insertSimpleCodeTag');
 
     editor.ui.addButton('SimpleCodeTag', {
       label: 'Insert Code Tag',
@@ -25,14 +32,3 @@ CKEDITOR.plugins.add('simplecodetag', {
     });
   }
 });
-
-/* CSS
-
-//color: #242729;
-color: rgb(255 255 255);
-//background-color: #e4e6e8;
-background-color: #2c3e50;
-padding: 2px 4px;
-border-radius: 2px;
-
-*/
