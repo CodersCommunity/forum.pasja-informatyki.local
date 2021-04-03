aaf335cbe7e6ab135190bafe6c8ff96c22f79221
mwos <mwos@getresponse.com>
2021-04-03 21:10:09 +0200

    Dodanie js

--- forum/qa-plugin/q2a-change-username-limit/q2a-changeusernamelimit-layer.php
+++ forum/qa-plugin/q2a-change-username-limit/q2a-changeusernamelimit-layer.php
@@ -7,13 +7,12 @@ class qa_html_theme_layer extends qa_html_theme_base
         parent::head_css();
 
         $this->output( '<link rel="stylesheet" href="' . qa_path_to_root() . 'qa-plugin/q2a-change-username-limit/css/style.css?v=' . QA_RESOURCE_VERSION . '">' );
-        $this->output( '<link rel="stylesheet" href="' . qa_path_to_root() . 'qa-plugin/q2a-change-username-limit/css/style.css?v=' . QA_RESOURCE_VERSION . '">' );
     }
 
     public function head_script()
     {
         if (in_array($this->template, ['user'])) {
-            $path = qa_html(qa_path_to_root() . 'js/timeline.js?v=' . QA_RESOURCE_VERSION);
+            $path = qa_html(qa_path_to_root() . 'qa-plugin/q2a-change-username-limit/js/timeline.js?v=' . QA_RESOURCE_VERSION);
             $this->content['script'][] = '<script src="' . $path . '" defer></script>';
         }
 
