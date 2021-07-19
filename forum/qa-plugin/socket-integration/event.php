e098bc4f5cde4b934f225d255ebb6bb9012027d5
Arkadiusz Waluk <arkadiusz@waluk.pl>
2021-07-19 19:47:43 +0200

    Add sending details and recipients for event a_post and c_post

--- forum/qa-plugin/socket-integration/event.php
+++ forum/qa-plugin/socket-integration/event.php
@@ -16,33 +16,79 @@ class socket_integration_event
             case 'q_close':
             case 'q_reopen':
             case 'q_move':
-                $this->send_to_websocket($event, ['question_id' => (int)$params['postid']]);
+                $this->send_to_websocket($event, ['questionId' => (int)$params['postid']]);
                 break;
-            case 'a_post':
             case 'a_edit':
             case 'a_hide':
             case 'a_reshow':
             case 'a_select':
             case 'a_unselect':
-                $this->send_to_websocket($event, ['question_id' => (int)$params['parentid']]);
+                $this->send_to_websocket($event, ['questionId' => (int)$params['parentid']]);
                 break;
-            case 'c_post':
             case 'c_edit':
             case 'c_hide':
             case 'c_reshow':
             case 'a_to_c':
-                $this->send_to_websocket($event, ['question_id' => (int)$params['questionid']]);
+                $this->send_to_websocket($event, ['questionId' => (int)$params['questionid']]);
+                break;
+            case 'a_post':
+            case 'c_post':
+                $questionParams = $params['question'] ?? $params['parent'];
+                $slug = explode('/', qa_q_request($questionParams['postid'], $questionParams['title']))[1] ?? null;
+
+                $this->get_recipients($event, $params);
+                $this->send_to_websocket($event, [
+                    'questionId' => (int)$questionParams['postid'],
+                    'questionTitle' => $slug,
+                    'postId' => (int)$params['postid'],
+                    'url' => qa_q_path(
+                        $questionParams['postid'],
+                        $questionParams['title'],
+                        true,
+                        isset($params['question']) ? 'C' : 'A',
+                        $params['postid']
+                    )
+                ], $this->get_recipients($event, $params));
                 break;
         }
     }
 
-    private function send_to_websocket($action, $data = [])
+    private function get_recipients($event, $params = [])
+    {
+        $users = [];
+        switch ($event) {
+            case 'a_post':
+                if (!empty($params['parent']['userid'])) {
+                    $users = [$params['parent']['userid']];
+                }
+                break;
+            case 'c_post':
+                $users = qa_db_read_all_values(qa_db_query_sub(
+                    'SELECT DISTINCT userid FROM `^posts` WHERE `parentid` = # AND `type` = "C" AND `userid` IS NOT NULL',
+                    $params['parentid']
+                ));
+                if (!empty($params['parent']['userid'])) {
+                    $users[] = $params['parent']['userid'];
+                }
+                break;
+        }
+
+        return array_values(array_filter(array_map('intval', $users), function ($id) {
+            return $id !== (int)qa_get_logged_in_userid();
+        }));
+    }
+
+    private function send_to_websocket($action, $data = [], $recipientIds = [])
     {
         $options = [
             'http' => [
                 'header' => "Content-Type: application/json\r\n" . "token: " . QA_WS_TOKEN . "\r\n",
                 'method' => 'POST',
-                'content' => json_encode(['action' => $action, 'data' => $data], JSON_UNESCAPED_UNICODE)
+                'content' => json_encode([
+                    'action' => $action,
+                    'recipientIds' => $recipientIds,
+                    'data' => $data,
+                ], JSON_UNESCAPED_UNICODE)
             ]
         ];
 
