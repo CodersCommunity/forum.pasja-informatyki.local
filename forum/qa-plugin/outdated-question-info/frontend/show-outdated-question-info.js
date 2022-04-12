1c3a13efb513dc9d011a681ccf26b94a1f57eba3
wizarddos <wizardkol123@gmail.com>
2022-04-12 20:33:49 +0200

    Feature - Added valid info about question

--- forum/qa-plugin/outdated-question-info/frontend/show-outdated-question-info.js
+++ forum/qa-plugin/outdated-question-info/frontend/show-outdated-question-info.js
@@ -1,16 +1,30 @@
+const showInfoAboutOutdatedQuestion = (placeOfOutdatedQuestionInfo) => {
+    const publishDateSpan = document.querySelectorAll('.value-title')[2];
 
-const placeOfOutdatedQuestionInfo = document.querySelector('.qa-a-form');
+    const now = new Date();
+    const publishDate = new Date(publishDateSpan.title);
 
-const publishDateSpan = document.querySelectorAll('.value-title')[2];
-
-const now = new Date();
-const publishDate = new Date(publishDateSpan.title);
-//TODO: Fix this
-console.log(publishDate.getMonth()-1 >= now.getMonth());
-if(publishDate.getFullYear() < now.getFullYear()) {
-    if(publishDate.getMonth()-2 > now.getMonth()){
-        placeOfOutdatedQuestionInfo.insertAdjacentHTML('beforebegin', `<div><h2>Hello</h2></div>`);
+    if(publishDate.getFullYear() < now.getFullYear()) {
+        if(publishDate.getMonth()-1 >= now.getMonth()){
+            if(!document.querySelector('.qa-outdated-question')) {
+                placeOfOutdatedQuestionInfo.insertAdjacentHTML('beforebegin', 
+                        `<div class = "qa-outdated-question-container">
+                                <span class = "qa-outdated-question-info">
+                                        To pytanie zostało zadane ponad 2 miesiące temu i może być już nie aktualne.<br/>
+                                        Zastanów się, czy na pewno chcesz "odkopać" to pytanie.
+                                </span>
+                        </div>`);
+            }
+        }
     }
-}else{
-    placeOfOutdatedQuestionInfo.insertAdjacentHTML('beforebegin', `<div><h2>Bye</h2></div>`);
+}
+if(document.querySelector('.qa-a-form')!== null){
+    const placeOfOutdatedQuestionInfo = document.querySelector('.qa-a-form')
+    if(placeOfOutdatedQuestionInfo.getAttribute('style') === "display:none;"){
+        document.querySelector('#q_doanswer').addEventListener('click', ()=>{
+            showInfoAboutOutdatedQuestion(placeOfOutdatedQuestionInfo)
+        });
+    }else{
+        showInfoAboutOutdatedQuestion(placeOfOutdatedQuestionInfo);
+    }
 }
--- forum/qa-plugin/outdated-question-info/qa-outdated-question-info-layer.php
+++ forum/qa-plugin/outdated-question-info/qa-outdated-question-info-layer.php
@@ -4,6 +4,9 @@ class qa_html_theme_layer extends qa_html_theme_base
 {
     public function head_script(){
         parent::head_script();
-        $this->output('<script src = "'. QA_HTML_THEME_LAYER_URLTOROOT .'frontend/show-outdated-question-info.js?v=" defer></script>');
+        $this->output('
+            <script src = "'. QA_HTML_THEME_LAYER_URLTOROOT .'frontend/show-outdated-question-info.js?v=" defer></script>
+            <link rel = "stylesheet" href = "'. QA_HTML_THEME_LAYER_URLTOROOT .'frontend/css/styles.css?v=" />
+        ');
     }
 }
