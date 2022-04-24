dd46a93d643786d96a37e7833258bda406d0953a
wizarddos <wizardkol123@gmail.com>
2022-04-24 12:30:14 +0200

    Fix - fix according to pr suggestions

--- forum/qa-plugin/outdated-question-info/frontend/show-outdated-question-info.js
+++ forum/qa-plugin/outdated-question-info/frontend/show-outdated-question-info.js
@@ -1,27 +1,29 @@
-const showInfoAboutOutdatedQuestion = () => {
+const placeOfOutdatedQuestionInfo = document.querySelector('.qa-a-form');
+if(placeOfOutdatedQuestionInfo){
     const publishDateSpan = document.querySelector('.published > .value-title');
-    const placeOfOutdatedQuestionInfo = document.querySelector('.qa-a-form');
-
     const now = new Date();
     const publishDate = new Date(publishDateSpan.title);
-
     const publishYearOlderThanNow = publishDate.getFullYear() < now.getFullYear();
     const publishMonthNewerThanNow = publishDate.getMonth() - 1 >= now.getMonth();
-    const doesQuestionElemExist = document.querySelector('.qa-outdated-question-container');
 
-    if (publishYearOlderThanNow && publishMonthNewerThanNow && !doesQuestionElemExist) {
+    if (publishYearOlderThanNow && publishMonthNewerThanNow) {
+        let infoDisplay = "";
+        if(placeOfOutdatedQuestionInfo.style.display !== 'none'){
+            infoDisplay = "display:block;";
+        }else{
+            infoDisplay = "display:none;";
+        }
         placeOfOutdatedQuestionInfo.insertAdjacentHTML('beforebegin', 
-                `<p class = "qa-outdated-question-container">
-                        To pytanie zostało zadane ponad 2 miesiące temu i może być już nieaktualne.<br/>
-                        Zastanów się, czy na pewno chcesz "odkopać" to pytanie.
+                `<p class = "qa-outdated-question-container" style = "${infoDisplay}">
+                    To pytanie zostało zadane już dawno temu i może być nieaktualne.<br/>
+                    Upewnij się, że Twoja odpowiedź nadal będzie pomocna.
                 </p>`);
     }
-}
-if(document.querySelector('.qa-a-form')){
-    const placeOfOutdatedQuestionInfo = document.querySelector('.qa-a-form');
-    if(placeOfOutdatedQuestionInfo.getAttribute('style') === "display:none;"){
-        document.querySelector('#q_doanswer').addEventListener('click', showInfoAboutOutdatedQuestion);
-    }else{
-        showInfoAboutOutdatedQuestion();
+    const QuestionElemExist = document.querySelector('.qa-outdated-question-container');
+
+    if(QuestionElemExist){
+        document.querySelector('#q_doanswer').addEventListener('click', ()=>{
+            QuestionElemExist.style.display = (QuestionElemExist.style.display === 'block') ? 'none' : 'block';
+        })
     }
 }
